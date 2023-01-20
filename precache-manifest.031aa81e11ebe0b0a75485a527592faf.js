6b0c912739f50a8d44daeef46e103a337a363836
AnnaIrkhina <anka.irhina@gmail.com>
2023-01-19 17:37:43 -0800

    Updates

---
+++ precache-manifest.031aa81e11ebe0b0a75485a527592faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fe07820d367cc326898e2075551ab2",
+    "url": "/redux_cunban/index.html"
+  },
+  {
+    "revision": "721baf4abc7d49b4b4d4",
+    "url": "/redux_cunban/static/css/main.416f0806.chunk.css"
+  },
+  {
+    "revision": "5154877f22b1cc611a5a",
+    "url": "/redux_cunban/static/js/2.46e4a484.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/redux_cunban/static/js/2.46e4a484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721baf4abc7d49b4b4d4",
+    "url": "/redux_cunban/static/js/main.44166b80.chunk.js"
+  },
+  {
+    "revision": "5ea590a4ada7071c3460",
+    "url": "/redux_cunban/static/js/runtime-main.8b5eb240.js"
+  }
+]);
